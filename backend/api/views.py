40af37375fe2903be5653ef760fd88e671a617ef
Nohamorim <myamorimads2023@gmail.com>
2025-06-08 14:30:01 -0300

    Criação da View

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Book
+from .serializers import BookSerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'isbn']
+    
