5ce6a65d63b6f441ee427bd1336260516e546a2c
Nohamorim <myamorimads2023@gmail.com>
2025-08-20 23:33:27 -0300

    feat: add domain entities (Book, Author, User)

---
+++ src/LibraryFlow.Domain/Entities/Author.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Domain.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ src/LibraryFlow.Domain/Entities/Book.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int PublicationYear { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
