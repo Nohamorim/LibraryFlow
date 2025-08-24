819041a81c5b004838220a47b0531e2e8ae37199
Nohamorim <myamorimads2023@gmail.com>
2025-08-24 16:22:39 -0300

    feat(frontend): Configure Users module and components

--- src/LibraryFlow.Frontend/src/app/app.routes.ts
+++ src/LibraryFlow.Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.routes').then(m => m.routes)
+  },
+  {
+    path: 'authors',
+    loadChildren: () => import('./authors/authors.routes').then(m => m.routes)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./users/users.routes').then(m => m.routes)
+  },
+  { path: '', redirectTo: 'books', pathMatch: 'full' }
+];
---
+++ src/LibraryFlow.Frontend/src/app/users/users.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', component: UserListComponent },
+      { path: 'add', component: UserFormComponent },
+      { path: 'edit/:id', component: UserFormComponent }
+    ]
+  }
+];
