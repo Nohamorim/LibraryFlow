6e24df4ae006423687557fd1aff57237db9e64ac
Nohamorim <myamorimads2023@gmail.com>
2025-08-27 20:59:37 -0300

    feat(authors): Implement Author service with CRUD operations

--- src/LibraryFlow.Frontend/src/app/services/author.service.ts
+++ src/LibraryFlow.Frontend/src/app/services/author.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Author } from '../models/author.model';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,19 @@ export class AuthorService {
     return this.http.get<Author[]>(this.apiUrl);
   }
 
-  // Implement other methods (getById, create, update, delete)
+  getById(id: number): Observable<Author> {
+    return this.http.get<Author>(`${this.apiUrl}/${id}`);
+  }
+
+  add(author: Author): Observable<Author> {
+    return this.http.post<Author>(this.apiUrl, author);
+  }
+
+  update(id: number, author: Author): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, author);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
