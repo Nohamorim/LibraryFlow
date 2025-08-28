49d0474cafa16d4593a326a61b9d2283d1693dbc
Nohamorim <myamorimads2023@gmail.com>
2025-08-27 21:20:58 -0300

    feat(app): Implement AuthorService

--- src/LibraryFlow.Application/Services/AuthorService.cs
+++ src/LibraryFlow.Application/Services/AuthorService.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using LibraryFlow.Application.DTOs;
+using LibraryFlow.Domain.Entities;
 using LibraryFlow.Domain.Interfaces;
 
 namespace LibraryFlow.Application.Services
