6229cb81a69c469aca5966744d9abbdac943dd7b
Nohamorim <myamorimads2023@gmail.com>
2025-08-27 21:18:41 -0300

    feat(domain): Add Author entity

--- src/LibraryFlow.Domain/Entities/Author.cs
+++ src/LibraryFlow.Domain/Entities/Author.cs
@@ -9,7 +9,7 @@ namespace LibraryFlow.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Biography { get; set; }
+        //public string Biography { get; set; }
         public ICollection<Book>? Books { get; set; }
     }
 }
