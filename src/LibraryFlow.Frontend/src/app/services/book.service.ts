96853a1abe541616153f75d3bde471da8e0da073
Nohamorim <myamorimads2023@gmail.com>
2025-08-27 21:00:01 -0300

    feat(authors): Implement Book service with CRUD operations

--- src/LibraryFlow.Frontend/src/app/services/book.service.ts
+++ src/LibraryFlow.Frontend/src/app/services/book.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book.model';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,19 @@ export class BookService {
     return this.http.get<Book[]>(this.apiUrl);
   }
 
-  // Implementar outros métodos (getById, create, update, delete)
+  getById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
+  add(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  update(id: number, book: Book): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, book);
+  }
+
+ delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
