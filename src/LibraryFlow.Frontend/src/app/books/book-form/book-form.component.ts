a6ab65e6c2531403ce1b8788eff3451cca14924b
Nohamorim <myamorimads2023@gmail.com>
2025-10-11 20:24:21 -0300

    feat: add form validations for book, author and user forms

--- src/LibraryFlow.Frontend/src/app/books/book-form/book-form.component.ts
+++ src/LibraryFlow.Frontend/src/app/books/book-form/book-form.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+// book-form.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-form',
-  imports: [],
   templateUrl: './book-form.component.html',
-  styleUrl: './book-form.component.scss'
+  styleUrls: ['./book-form.component.scss']
 })
-export class BookFormComponent {
+export class BookFormComponent implements OnInit {
+  bookForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      isbn: ['', [Validators.required, Validators.pattern(/^\d{10,13}$/)]],
+      authorId: ['', Validators.required],
+      publicationYear: ['', [Validators.required, Validators.min(1000), Validators.max(new Date().getFullYear())]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      stockQuantity: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  ngOnInit(): void {}
 }
